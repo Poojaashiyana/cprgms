c82fe9522e70536fe710651f1fa918db54f05c40
Pooja P P <poojaanilashiyana@yahoo.com>
2024-07-17 16:27:20 +0530

    Add files via upload

---
+++ ari 4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z,k;
+printf("Enter the value of x,y and z");
+scanf("%d%d%d",&x,&y,&z);
+k=x+y/z-y;
+printf("%d",y);
+return 0;
+}
---
+++ ari 6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+printf("Enter the value of x and y");
+scanf("%d%d",&x,&y,);
+z=x>y;
+printf("%d",z);
+return 0;
+}
